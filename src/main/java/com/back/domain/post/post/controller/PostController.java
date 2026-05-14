d0e075a7df3e32a7f31d0716340f9812dc9bdb32
puppywimy <eric250@naver.com>
2026-05-14 14:15:56 +0900

    4강: 글 작성 폼 추가

---
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.back.domain.post.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write() {
+        return """
+                <div style="display: flex; align-items: center; justify-content: center; height: 100%;">
+                    <form action="http://localhost:8080/posts/doWrite" method="POST">
+                        <input type="text" name="title" placeholder="제목" />
+                        <br />
+                        <textarea name="content" placeholder="내용"></textarea>
+                        <br />
+                        <input type="submit" value="작성" />
+                    </form>
+                </div>
+                """;
+    }
+}
