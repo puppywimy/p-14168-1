cf5d8c9a48b614b52bc5c0f2459afb6019fcb0c0
puppywimy <eric250@naver.com>
2026-05-19 14:31:38 +0900

    46강: Spring Security 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testAnnotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 }
 
 tasks.withType<Test> {
